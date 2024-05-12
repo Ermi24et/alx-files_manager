f6913e186e233f1a997798edae6da45cb934b97c
Ermi24et <lilermi24@gmail.com>
2024-05-12 21:28:04 +0300

    redis utils

---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.connected = true
+
+        this.client.on('error', (err) => {
+            console.log(`Redis Client Error, ${err.message}`);
+            this.connected = false;
+        });
+    }
+
+    isAlive() {
+        return this.connected;
+    }
+
+    async get(key) {
+        const getAsync = promisify(this.client.get).bind(this.client);
+        return getAsync(key);
+    }
+
+    async del(key) {
+        this.client.del(key);
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
