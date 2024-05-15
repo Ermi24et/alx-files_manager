669113463983a626a21bd16ca1674b8760cf6238
Ermi24et <lilermi24@gmail.com>
2024-05-16 00:53:51 +0300

    an index contains all routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app) {
@@ -29,6 +30,26 @@ function controllerRouting(app) {
 
   router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  router.get('/files/:id/date', (req, res) => {
+    FilesController.getFile(req, res);
   });
 }
 
